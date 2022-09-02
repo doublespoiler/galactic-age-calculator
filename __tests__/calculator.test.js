3d392ff96500179106bab58c9fb3840c83f7f05e
Skylan Lew <skylanslew@gmail.com>
2022-09-02 09:25:48 -0700

    reword test 1

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -2,7 +2,7 @@ import { TestScheduler } from 'jest';
 import Calculator from './../src/js/calculator.js';
 
 describe ('Calculator', () => {
-  test('it should create a calculator object with the input name in Earth years.', () =>{
+  test('it should create a calculator object with the input in Earth years.', () =>{
     const earthYearsCalc = new Calculator("21");
     expect(earthYearsCalc.earthYears).toEqual(21);
   });
