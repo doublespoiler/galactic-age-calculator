9cdc0159ec9afd7fa898035efd3dafa9abaf8f8b
Skylan Lew <skylanslew@gmail.com>
2022-09-02 15:37:11 -0700

    GREEN pass Calculator test 5

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -7,6 +7,10 @@ export default class Calculator{
     this.currentYear = currentDate.getFullYear();
     this.currentMonth = currentDate.getMonth();
     this.currentDay = currentDate.getDate();
-    this.daysSinceBirth = ((this.currentYear - this.birthYear)*365) + (((this.currentMonth - this.birthMonth)*30)) + (this.currentDay - this.birthDay);
+    this.daysSinceBirth = ((this.currentYear - this.birthYear)*365) + (((this.currentMonth - this.birthMonth)*30)) + (this.currentDay - this.birthDay); //age in days
+    this.years = parseInt((this.daysSinceBirth/365.00));
+
   }
+
+  //planet years is planet age * 
 }
