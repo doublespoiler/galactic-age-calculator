0cdfcfd9e6d48cff2a95c948f4834e346703a0d7
Skylan Lew <skylanslew@gmail.com>
2022-09-02 09:11:34 -0700

    good fail Calculator test 1

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,9 @@
+import { TestScheduler } from 'jest';
+import Calculator from './../src/js/calculator.js';
+
+describe ('Calculator', () => {
+  test('it should create a calculator object with the input name in Earth years.', () =>{
+    const earthYearsCalc = new Calculator("21");
+    expect(earthYearsCalc.earthYears).toEqual(21);
+  });
+});
