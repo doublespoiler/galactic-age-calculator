cb0200bb97d3548e3f5ac911c7bf4684b939a7b6
Skylan Lew <skylanslew@gmail.com>
2022-09-02 09:39:50 -0700

    refactor Calculator test 1, good fail

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -3,9 +3,9 @@ import Calculator from './../src/js/calculator.js';
 
 describe ('Calculator', () => {
   test('it should create a calculator object with the input birth date in Earth Years, Months and Days.', () =>{
-    const earthYearsCalc = new Calculator("4");
+    const earthYearsCalc = new Calculator("4", "21", "1992");
     expect(earthYearsCalc.birthMonth).toEqual(4);
     expect(earthYearsCalc.birthDay).toEqual(21);
-    expect(earthYearsCalc.birthYears).toEqual(1992);
+    expect(earthYearsCalc.birthYear).toEqual(1992);
   });
 });
