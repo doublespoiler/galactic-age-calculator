15be0e8948a4c0499aea4a0a6af26a3b6d5f5ea3
Skylan Lew <skylanslew@gmail.com>
2022-09-11 13:31:46 -0700

    refactor calculator.js, put all calculations in their own function

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -6,20 +6,21 @@ let earthYearsCalc;
 
 beforeEach(() => {
   currentDate = new Date("2022-09-02T00:00:00.000Z");
-  earthYearsCalc = new Calculator("4", "21", "1992", currentDate);
+  earthYearsCalc = new Calculator("4", "2", "1992", currentDate);
+  earthYearsCalc.planetCalc();
   
 });
 
 describe ('Calculator', () => {
   test('it should create a calculator object with the input birth date in Earth Years, Months and Days.', () =>{
-
+    const earthYearsCalc = new Calculator("4", "21", "1992", currentDate);
     expect(earthYearsCalc.birthMonth).toEqual(4);
     expect(earthYearsCalc.birthDay).toEqual(21);
     expect(earthYearsCalc.birthYear).toEqual(1992);
   });
 
   test('it should store the current date in ms since Jan 1 1970', () => {
-
+    const earthYearsCalc = new Calculator("4", "21", "1992", currentDate);
     expect(earthYearsCalc.currentDate).toEqual(currentDate);
   });
 
@@ -31,12 +32,10 @@ describe ('Calculator', () => {
   });
 
   test('it should set the total days since birth', () =>  {
-    const earthYearsCalc = new Calculator("4", "2", "1992", currentDate); 
     expect(earthYearsCalc.daysSinceBirth).toEqual(11069); //((2022 - 1992)*365) + ((8-4)*30) + ((1-2))
   });
 
   test('should set the year age of the user', () => {
-    const earthYearsCalc = new Calculator("4", "2", "1992", currentDate);
     expect(earthYearsCalc.years).toEqual(30);
   }); 
   //it was at this point I realized 
@@ -44,23 +43,20 @@ describe ('Calculator', () => {
   //I think I plan too far ahead, maybe.
 
   test('should return the age of the user on Mercury.', () => {
-    const earthYearsCalc = new Calculator("4", "2", "1992", currentDate);
-    expect(earthYearsCalc.mercuryYears()).toEqual(125);
+    expect(earthYearsCalc.mercuryYears).toEqual(125);
   });
 
   test('should return the age of the user on Venus', () => {
-    const earthYearsCalc = new Calculator("4", "2", "1992", currentDate);
-    expect(earthYearsCalc.venusYears()).toEqual(48.39);
+    expect(earthYearsCalc.venusYears).toEqual(48.39);
   });
 
   test('should return the age of the user on Mars', () => {
-    const earthYearsCalc = new Calculator("4", "2", "1992", currentDate);
-    expect(earthYearsCalc.marsYears()).toEqual(15.96);
+    expect(earthYearsCalc.marsYears).toEqual(15.96);
   });
 
   test('should return the age of the user on Jupiter', () => {
-    const earthYearsCalc = new Calculator("4", "2", "1992", currentDate);
-    expect(earthYearsCalc.jupiterYears()).toEqual(2.53);
+    expect(earthYearsCalc.jupiterYears).toEqual(2.53);
   });
+
 
 });
